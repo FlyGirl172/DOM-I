5308ce6e0fecf8cef6a6101c48d3f02cd0bfe085
Joanna Rogers <FlyGirl172s@gmail.com>
2023-07-30 17:48:03 -0500

    Manipulating the DOM using ONLY the index.js file- Finished/ Passing all tests!

--- src/index.js
+++ src/index.js
@@ -40,3 +40,71 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
 };
 
 console.log('project wired!')
+
+
+
+//Images
+const logoImage = document.querySelector('#logo-img');
+logoImage.src = siteContent.images['logo-img'];
+
+const ctaImage = document.querySelector('#cta-img');
+ctaImage.src = siteContent.images['cta-img'];
+
+const middleImage = document.querySelector('#middle-img');
+middleImage.setAttribute('src', siteContent.images['accent-img']); 
+
+//Footer Link
+const footLink = document.querySelector('footer a');
+//console.log(footLink); //<a href="#"></a>
+footLink.textContent = siteContent.footer.copyright;
+footLink.classList.add('bold');
+
+//Contact
+const contactSection = document.querySelector('section.contact');
+//contactSection[0].textContent = siteContent.contact['contact-h4'];
+contactSection.querySelector('h4').textContent = siteContent.contact['contact-h4'];
+
+contactSection.querySelector('p:nth-of-type(1)').textContent = siteContent.contact['address'];
+contactSection.querySelector('p:nth-of-type(2)').textContent = siteContent.contact['phone'];
+contactSection.querySelector('p:nth-of-type(3)').textContent = siteContent.contact['email'];
+// contactSection[1].textContent = siteContent.contact['address'];
+// contactSection[2].textContent = siteContent.contact['phone'];
+// contactSection[4].textContent = siteContent.contact['email'];
+
+//Content at the Top
+const topContent = document.querySelector('.top-content');
+//console.log(topContent); //<div class="top-content">
+topContent.children[0].children[0].textContent = siteContent['main-content']['features-h4'];
+topContent.children[0].children[1].textContent = siteContent['main-content']['features-content'];
+topContent.children[1].children[0].textContent = siteContent['main-content']['about-h4'];
+topContent.children[1].children[1].textContent = siteContent['main-content']['about-content'];
+
+//Content at the Bottom
+const bottomContent = document.querySelector('.bottom-content');
+//bottomContent.textContent = siteContent['main-content']; ///??????*********
+
+const h4Bottom = bottomContent.querySelectorAll('h4');
+//console.log(h4Bottom);  //NodeList(6)[h4, h4, h4, h4, h4, h4]
+h4Bottom[0].textContent = siteContent['main-content']['services-h4'];
+h4Bottom[1].textContent = siteContent['main-content']['product-h4'];
+h4Bottom[2].textContent = siteContent['main-content']['vision-h4'];
+
+const psBottom = bottomContent.querySelectorAll('p');
+psBottom[0].textContent = siteContent['main-content']['services-content'];
+psBottom[1].textContent = siteContent['main-content']['product-content'];
+psBottom[2].textContent = siteContent['main-content']['vision-content'];
+
+//CTA (Call TO Action)
+document.querySelector('.cta .cta-text h1').textContent = siteContent.cta.h1;
+document.querySelector('.cta .cta-text button').textContent = siteContent.cta.button;
+
+//Nav
+const navTop = document.querySelectorAll('header nav a');
+const navTopTitles = Object.values(siteContent.nav);
+navTop.forEach((link, idx) => {
+   link.textContent = navTopTitles[idx]
+   link.classList.add('italic');
+});
+
+
+
